b7698b0b70d6cb06d58ea837b00227d3445c2a8f
Miloš Roknić <roknic.milos.994@gmail.com>
2025-06-04 08:33:45 +0200

    Connect FE and BE

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,39 @@
+"use client"
+
+import {useEffect, useState} from "react";
+
 export default function Home() {
-  return (
-    <div>TODO: DISPLAY ACTUAL HOME PAGE CONTENT</div>
-  );
+    const [content, setContent] = useState<object | null>(null);
+
+    useEffect(() => {
+        if (!content) {
+            fetchHomePageData()
+        }
+    }, [content]);
+
+    async function fetchHomePageData() {
+        try {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/home-page`, {
+                method: "GET",
+                headers: {
+                    "Authorization": `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
+                    "Content-Type": "application/json",
+                },
+            });
+            const data = await response.json();
+            setContent(data);
+        } catch (error) {
+            setContent({error: error.message})
+        }
+    }
+
+    if (!content) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            <pre><code>{JSON.stringify(content, null, 2)}</code></pre>
+        </div>
+    );
 }

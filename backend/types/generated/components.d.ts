4bbec2cbcfab35d7e66f744bdc8fca9184584302
nikolakokic95 <nikola.kokic@vegait.rs>
2025-06-13 22:53:51 +0200

    BE-Add Header and Footer for Layout

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,3 +1,101 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface LayoutFooter extends Struct.ComponentSchema {
+  collectionName: 'components_layout_footers';
+  info: {
+    displayName: 'footer';
+    icon: 'write';
+  };
+  attributes: {
+    emailLink: Schema.Attribute.Component<'shared.link', false>;
+    footerLogo: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios'
+    >;
+    footerSocialLinks: Schema.Attribute.Component<'shared.social-links', true>;
+    footerSubtitle: Schema.Attribute.String;
+    footerTitle: Schema.Attribute.String;
+    listLinks: Schema.Attribute.Component<'shared.list-links', false>;
+    policy: Schema.Attribute.Component<'shared.file-link', true> &
+      Schema.Attribute.SetMinMax<
+        {
+          max: 2;
+        },
+        number
+      >;
+  };
+}
+
+export interface LayoutHeader extends Struct.ComponentSchema {
+  collectionName: 'components_layout_headers';
+  info: {
+    displayName: 'header';
+    icon: 'doctor';
+  };
+  attributes: {
+    headerLogo: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios'
+    >;
+    listLinks: Schema.Attribute.Component<'shared.list-links', false>;
+  };
+}
+
+export interface SharedFileLink extends Struct.ComponentSchema {
+  collectionName: 'components_shared_file_links';
+  info: {
+    displayName: 'fileLink';
+    icon: 'cast';
+  };
+  attributes: {
+    file: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    label: Schema.Attribute.String;
+  };
+}
+
+export interface SharedLink extends Struct.ComponentSchema {
+  collectionName: 'components_shared_links';
+  info: {
+    displayName: 'Link';
+    icon: 'alien';
+  };
+  attributes: {
+    href: Schema.Attribute.String;
+    label: Schema.Attribute.String;
+  };
+}
+
+export interface SharedListLinks extends Struct.ComponentSchema {
+  collectionName: 'components_shared_list_links';
+  info: {
+    displayName: 'listLinks';
+    icon: 'apps';
+  };
+  attributes: {
+    links: Schema.Attribute.Component<'shared.link', true>;
+  };
+}
+
+export interface SharedSocialLinks extends Struct.ComponentSchema {
+  collectionName: 'components_shared_social_links';
+  info: {
+    displayName: 'socialLinks';
+    icon: 'attachment';
+  };
+  attributes: {
+    altText: Schema.Attribute.String;
+    href: Schema.Attribute.String;
+    socialNetwork: Schema.Attribute.Enumeration<['instagram', 'facebook']>;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'layout.footer': LayoutFooter;
+      'layout.header': LayoutHeader;
+      'shared.file-link': SharedFileLink;
+      'shared.link': SharedLink;
+      'shared.list-links': SharedListLinks;
+      'shared.social-links': SharedSocialLinks;
+    }
+  }
+}
--- backend/types/generated/contentTypes.d.ts
+++ backend/types/generated/contentTypes.d.ts
@@ -458,6 +458,35 @@ export interface ApiHomePageHomePage extends Struct.SingleTypeSchema {
   };
 }
 
+export interface ApiLayoutLayout extends Struct.SingleTypeSchema {
+  collectionName: 'layouts';
+  info: {
+    displayName: 'Layout';
+    pluralName: 'layouts';
+    singularName: 'layout';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    createdAt: Schema.Attribute.DateTime;
+    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    footer: Schema.Attribute.Component<'layout.footer', false>;
+    header: Schema.Attribute.Component<'layout.header', false>;
+    locale: Schema.Attribute.String & Schema.Attribute.Private;
+    localizations: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::layout.layout'
+    > &
+      Schema.Attribute.Private;
+    publishedAt: Schema.Attribute.DateTime;
+    updatedAt: Schema.Attribute.DateTime;
+    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+  };
+}
+
 export interface PluginContentReleasesRelease
   extends Struct.CollectionTypeSchema {
   collectionName: 'strapi_releases';
@@ -970,6 +999,7 @@ declare module '@strapi/strapi' {
       'api::about-us-page.about-us-page': ApiAboutUsPageAboutUsPage;
       'api::contact-page.contact-page': ApiContactPageContactPage;
       'api::home-page.home-page': ApiHomePageHomePage;
+      'api::layout.layout': ApiLayoutLayout;
       'plugin::content-releases.release': PluginContentReleasesRelease;
       'plugin::content-releases.release-action': PluginContentReleasesReleaseAction;
       'plugin::i18n.locale': PluginI18NLocale;

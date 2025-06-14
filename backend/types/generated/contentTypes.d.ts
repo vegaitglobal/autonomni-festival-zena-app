e98044b9382a0d4695e717331da2247fd44b9d22
Boris Denkovic <boris.denkovic@vegait.rs>
2025-06-14 13:20:34 +0200

    Updated content attributes to contact and about us content types

--- backend/types/generated/contentTypes.d.ts
+++ backend/types/generated/contentTypes.d.ts
@@ -384,7 +384,9 @@ export interface ApiAboutUsPageAboutUsPage extends Struct.SingleTypeSchema {
     draftAndPublish: true;
   };
   attributes: {
-    content: Schema.Attribute.RichText;
+    components: Schema.Attribute.DynamicZone<
+      ['page-components.large-rich-text']
+    >;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
@@ -412,7 +414,9 @@ export interface ApiContactPageContactPage extends Struct.SingleTypeSchema {
     draftAndPublish: true;
   };
   attributes: {
-    content: Schema.Attribute.RichText;
+    components: Schema.Attribute.DynamicZone<
+      ['page-components.small-rich-text']
+    >;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;

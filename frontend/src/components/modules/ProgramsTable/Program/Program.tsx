c1ba60867e235234bb0efafa7e674d39c1c4ff64
Miloš Roknić <39025384+roknicmilos@users.noreply.github.com>
2025-06-14 21:09:37 +0200

    FE | Implement Programs page (#35)
    * FE | Implement Programs page content
    
    * FE | Add title and 'go to' arrow on Program card

---
+++ frontend/src/components/modules/ProgramsTable/Program/Program.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { Program as ProgramModel } from '@/types/apiModels/Program';
+import arrowRight from '@/assets/arrow-right.png';
+import Image from 'next/image';
+import './Program.scss';
+
+interface ProgramCardProps {
+	program: ProgramModel;
+	color: 'green' | 'yellow';
+}
+
+export default function Program({ program, color }: ProgramCardProps) {
+	return (
+		<a className={`program-card program-card--${color}`} href="#">
+			<div className="program-card__header">
+				<p className="program-card__year">{program.year}</p>
+				<p>TODO: program. aboutProgram. title</p>
+			</div>
+			<p className="program-card__description">
+				TODO: program. aboutProgram. description program. aboutProgram. description
+				program. aboutProgram. description program. aboutProgram. description
+				program. aboutProgram. description program. aboutProgram. description
+				dfasdfasdf
+			</p>
+			<Image
+				className="program-card__arrow"
+				src={arrowRight}
+				alt="arrow"
+				width={50}
+				height={25}
+			/>
+		</a>
+	);
+}
---
+++ frontend/src/components/modules/ProgramsTable/ProgramsTable.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import Program from '@/components/modules/ProgramsTable/Program/Program';
+import { Program as ProgramModel } from '@/types/apiModels/Program';
+import './ProgramsTable.scss';
+
+interface ProgramsTableProps {
+	programs: ProgramModel[];
+}
+
+export default function ProgramsTable({ programs }: ProgramsTableProps) {
+	return (
+		<>
+			<div className="programs-table">
+				<h1 className="programs__title">PROGRAMI</h1>
+				{programs.map((program, index) => {
+					const color = index % 2 ? 'green' : 'yellow';
+					return <Program program={program} key={program.year} color={color} />;
+				})}
+			</div>
+		</>
+	);
+}

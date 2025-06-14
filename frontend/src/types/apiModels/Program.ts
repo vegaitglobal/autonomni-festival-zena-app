4ddcb4a669602d71c913fef2db324bddfdab93f0
Miloš Roknić <39025384+roknicmilos@users.noreply.github.com>
2025-06-14 23:05:59 +0200

    FE | Add bg image to Products page (#39)
    * FE | Implement Programs page content
    
    * FE | Add title and 'go to' arrow on Program card
    
    * FE | Add bg image to Programs page
    
    * Update frontend/src/components/modules/ProgramsTable/ProgramsTable.tsx
    
    * Update frontend/src/types/apiModels/Program.ts
    
    * Update frontend/src/components/modules/ProgramsTable/Program/Program.tsx
    
    * Fix typos

--- frontend/src/types/apiModels/Program.ts
+++ frontend/src/types/apiModels/Program.ts
@@ -3,6 +3,11 @@ import { ComponentData } from '../dynamicContent';
 
 type ProgramTheme = 'green' | 'pink';
 
+export interface AboutProgram {
+	title: string;
+	description: string;
+}
+
 export interface Program extends BaseAPIModel {
 	year: string;
 	theme: ProgramTheme;

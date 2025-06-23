ae0b96bf659674b420b1f6d1b06c38b157e06fbe
Milos Roknic <m.roknic@vegait.rs>
2025-06-23 14:08:50 +0200

    Improve 'not found' page

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -6,12 +6,10 @@ import { fetchNotFoundPage } from '@/services/pageService';
 import { NotFoundPage as NotFoundPageModel } from '@/types/apiModels/NotFound';
 
 export default function NotFoundPage() {
-	const { content, loading, error } =
+	const { content, loading } =
 		useResource<NotFoundPageModel>(fetchNotFoundPage);
 
 	if (loading) return <div>Loading...</div>;
-	if (error) return <div>Error: {error}</div>;
-	if (!content) return <div>Server error. We will be back.</div>;
 
-	return <NotFound {...content} />;
+	return <NotFound content={content?.content} />;
 }
